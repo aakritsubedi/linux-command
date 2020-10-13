f310d4ed4f7a20eccec5ecd1dddd097dc0abd46d
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-10-13 08:40:16 +0545

    Add Navigation Compoenent

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <style>
+@import url('https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 @import "assets/css/style.css";
 @import "assets/css/reset.css";
 </style>
---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navigation">
+    <div class="nav-left">
+      <div class="tabs">
+        <span>Learning Linux Command</span>
+      </div>
+      <div class="icon">
+        <span class="fa fa-github"></span>
+        <span class="fa fa-code"></span>
+        <span class="fa fa-cubes"></span>
+      </div>
+      <div class="selected-tab">
+        <span class="fa fa-terminal"></span> {{ selectedTab }}
+        <span class="fa fa-caret-down" style="margin-left: 5px"></span>
+      </div>
+    </div>
+    <div class="nav-center">
+      <span class="fa fa-calendar" style="margin-right: 5px"></span>
+      {{ date }}
+    </div>
+    <div class="nav-right">
+      <span class="fa fa-volume-up"></span>
+      <span class="fa fa-wifi"></span>
+      <span class="fa fa-battery-three-quarters"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  props: ["selectedTab"],
+  data() {
+    return {
+      date: new Date().toJSON().slice(0, 10).replace(/-/g, "/"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "./../../assets/css/navigation.css";
+</style>
